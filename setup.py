159a84e6b613f61f4ca4b85bca559927f69fee24
Sayandip Saha <145214083+SayandipSaha666@users.noreply.github.com>
2025-08-25 17:20:26 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Question Answer Generator",
+    version= '0.0.0',
+    author="Sayandip Saha",
+    author_email="sahasayandip480@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
